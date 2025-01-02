a8b2eba8be8fd95ad79303fe773da63ca530f080
artyomturkoff <art.turkoff@gmail.com>
2025-01-03 00:31:52 +0100

    Word count, character count and report added

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-def main():
+def content():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
+
+def count_words():
+    file_contents = content()
+    return len(file_contents.split())
+
+def count_characters():
+    file_contents = content()
+    lowered_text = file_contents.lower()
+    characters = dict()
+    for c in lowered_text:
+        if c in characters:
+            characters[c] += 1
+        else:
+            characters[c] = 1
+    return characters
+
+def report():
+    print("--- Begin report ---")
+    print(f"{count_words()} words found in the document\n")
+
+    sorted_characters = dict(sorted(count_characters().items(), key=lambda item: item[1], reverse=True)) 
+    for c in sorted_characters:
+        if c.isalpha():
+            print(f"The '{c}' character was found {sorted_characters[c]} times")
+    print("--- End report ---")
+
+def main():
+    report()
 
 if __name__ == "__main__":
     main()
